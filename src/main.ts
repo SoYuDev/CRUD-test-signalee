d765a3ddb86c560d60743a424222c717a3ed76a6
Sxmpl3 <116309678+Sxmpl3@users.noreply.github.com>
2024-11-05 17:06:52 +0100

    Update main.ts
    Cors añadidos

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:4200'], // Aqui agregamos nuestros dominios autorizados, en la mayoria de caso agregaremos nuestras rutas para guardar credenciales, login, etc...
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metodos aceptados
+    allowedHeaders: ['Content-Type', 'Authorization'], // Los headers a utilizar, en este caso headers de autorización
+    credentials: true, // Habilitamos el envio de credenciales
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
